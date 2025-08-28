64100a7fa9a0f1d12a62f6f70bc01155c7d612b8
IlikeChooros <pawel.smolnicki@gmail.com>
2025-08-28 17:41:47 +0200

    Refactor battery level reading and update BLE initialization

--- bt_keyboard.ino
+++ bt_keyboard.ino
@@ -10,11 +10,13 @@ License: MIT
 
 */
 
+constexpr int SLEEP_TIMEOUT = 15 * 60 * 1000; // 15 minutes
+
 constexpr int PIN_BATTERY_LEVEL = 13,
               PIN_LED_BUILTIN = 2;
 
 constexpr int PIN_BUTTONS[] = {
-    14, 27, 25, 26
+    26, 25, 14, 27
 };
 
 BleKeyboard bleKeyboard;
@@ -32,8 +34,8 @@ void press() {
 }
 
 const internal::callback_t callbacks[] = {
-    press<KEY_UP_ARROW>,
     press<KEY_DOWN_ARROW>,
+    press<KEY_UP_ARROW>,
     press<KEY_BACKSPACE>,
     press<KEY_RETURN>
 };
@@ -43,24 +45,28 @@ void releaseAll() {
 }
 
 /**
- * Read the battery level and set the bt characteristic.
  * Using 47 + 100 k ohm voltage divider, with li ion 3.7v battery,
  * so 0% is about 3.4v and 100% is about 4.2v, with that voltage divider
- * I get 100/147 * 3.4v = 2.313v, and 100/147 * 4.2v = 2.857v
+ * I get 100/147 * 3.2v = 2.176v, and 100/147 * 4.1v = 2.789v
+ */
+uint8_t readBatteryLevel() {
+    constexpr int BATTERY_LEVEL_MIN_MV = 2176,
+                  BATTERY_LEVEL_MAX_MV = 2789;
+
+    int rawMv = analogReadMilliVolts(PIN_BATTERY_LEVEL);
+    int mapped = std::max(BATTERY_LEVEL_MIN_MV, std::min(BATTERY_LEVEL_MAX_MV, rawMv));
+    return uint8_t(float(mapped - BATTERY_LEVEL_MIN_MV) / float(BATTERY_LEVEL_MAX_MV - BATTERY_LEVEL_MIN_MV) * 100.0f);
+}
+
+/**
+ * Read the battery level and set the bt characteristic.
  */
 void batteryTask() {
-    constexpr int BATTERY_LEVEL_MIN_MV = 2313,
-                  BATTERY_LEVEL_MAX_MV = 2857;
-
     static int64_t lastUpdate = 0;
 
     if (millis() - lastUpdate > 10000) {
         lastUpdate = millis();
-        int rawMv = analogReadMilliVolts(PIN_BATTERY_LEVEL);
-        int mapped = std::max(BATTERY_LEVEL_MIN_MV, std::min(BATTERY_LEVEL_MAX_MV, rawMv));
-        int level = int(float(mapped - BATTERY_LEVEL_MIN_MV) / float(BATTERY_LEVEL_MAX_MV - BATTERY_LEVEL_MIN_MV) * 100.0f);
-
-        bleKeyboard.setBatteryLevel(level);
+        bleKeyboard.setBatteryLevel(readBatteryLevel());
     }
 }
 
@@ -69,7 +75,7 @@ void setup() {
     pinMode(PIN_LED_BUILTIN, OUTPUT);
     digitalWrite(PIN_LED_BUILTIN, HIGH);
 
-    bleKeyboard = BleKeyboard("Miniboard", "Pavlov sp. z o.o.", 50);
+    bleKeyboard = BleKeyboard("Miniboard", "Pavlov sp. z o.o.", readBatteryLevel());
     bleKeyboard.begin();
     Serial.begin(115200);
     
@@ -86,19 +92,23 @@ void setup() {
         buttons[i].onPress(callbacks[i]).onRelease(releaseAll);
 
         // wakeup if any button is pressed
-        // gpio_wakeup_enable(gpio_num_t(PIN_BUTTONS[i]), GPIO_INTR_LOW_LEVEL);
+        gpio_wakeup_enable(gpio_num_t(PIN_BUTTONS[i]), GPIO_INTR_LOW_LEVEL);
     }
 
-    // esp_sleep_enable_gpio_wakeup();
+    esp_sleep_enable_gpio_wakeup();
     Serial.println("BLE Keyboard started");
 }
 
-void loop(){
+void loop() {
     static int64_t lastActivity = 0;
 
-    // Enter deep sleep mode after 5 minutes of inactivity
-    if (millis() - lastActivity > 5 * 60 * 1000) {
-        // esp_deep_sleep_start();
+    // Enter deep sleep mode after given timeout
+    if (millis() - lastActivity > SLEEP_TIMEOUT) {
+        esp_light_sleep_start();
+        lastActivity = millis();
+
+        bleKeyboard.begin(); // causes memory leak but whatever :))
+        bleKeyboard.setBatteryLevel(readBatteryLevel());
     }
 
     if (bleKeyboard.isConnected()) {
